874f0944e8cf1b783d9cf8f83798470610e0bcd3
dumbledope <84159538+dumbledope@users.noreply.github.com>
2022-07-08 09:31:12 -0700

    feat: render.yaml update

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -20,78 +20,82 @@ import {
   balancesQueue,
 } from './queues';
 import { getBalances } from '../wallet';
-
-const client = createRedisClient();
+import { EventEmitter } from 'events';
 
 export function deserializeJob<T>(job: { data: { event: SerializedEvent<T> } }) {
   return deserializeEvent(job.data.event);
 }
 
-void finalizeInboundQueue.process(1, async job => {
-  const event = await deserializeJob(job);
-  return processFinalizedInbound(event, client);
-});
+export function initWorkerThread() {
+  EventEmitter.defaultMaxListeners = 30;
+  const client = createRedisClient();
 
-void sendOutboundQueue.process(1, async job => {
-  const event = await deserializeJob(job);
-  return processOutboundSwap(event, client);
-});
+  void finalizeInboundQueue.process(1, async job => {
+    const event = await deserializeJob(job);
+    return processFinalizedInbound(event, client);
+  });
 
-void eventQueue.process(async job => {
-  const event = await deserializeJob(job);
-  if (isFinalizeInboundEvent(event)) {
-    await finalizeInboundQueue.add({ event: serializeEvent(event) });
-  } else if (isInitiateOutboundEvent(event)) {
-    await sendOutboundQueue.add({ event: serializeEvent(event) });
-  }
-  return {
-    topic: event.print.topic,
-  };
-});
+  void sendOutboundQueue.process(1, async job => {
+    const event = await deserializeJob(job);
+    return processOutboundSwap(event, client);
+  });
 
-void eventCronQueue.process(1, async () => {
-  const lastSeenTxid = await getLastSeenTxid(client);
-  const newEvents = await getContractEventsUntil(lastSeenTxid);
-  if (newEvents.length > 0) {
-    const topics = newEvents.map(e => e.print.topic);
-    logger.debug({ topic: 'processEvents', topics }, `Processing ${newEvents.length} new events`);
-  }
-  const [firstEvent] = newEvents;
-  if (firstEvent !== undefined) {
-    await setLastSeenTxid(client, firstEvent.txid);
-  }
-  const eventJobs = newEvents.map(event => ({ data: { event: serializeEvent(event) } }));
-  await eventQueue.addBulk(eventJobs);
-  return {
-    newEvents: newEvents.length,
-    lastSeenTxid,
-  };
-});
+  void eventQueue.process(async job => {
+    const event = await deserializeJob(job);
+    if (isFinalizeInboundEvent(event)) {
+      await finalizeInboundQueue.add({ event: serializeEvent(event) });
+    } else if (isInitiateOutboundEvent(event)) {
+      await sendOutboundQueue.add({ event: serializeEvent(event) });
+    }
+    return {
+      topic: event.print.topic,
+    };
+  });
 
-void finalizeOutboundQueue.process(1, async () => {
-  return processPendingOutbounds(client);
-});
+  void eventCronQueue.process(1, async () => {
+    const lastSeenTxid = await getLastSeenTxid(client);
+    const newEvents = await getContractEventsUntil(lastSeenTxid);
+    if (newEvents.length > 0) {
+      const topics = newEvents.map(e => e.print.topic);
+      logger.debug({ topic: 'processEvents', topics }, `Processing ${newEvents.length} new events`);
+    }
+    const [firstEvent] = newEvents;
+    if (firstEvent !== undefined) {
+      await setLastSeenTxid(client, firstEvent.txid);
+    }
+    const eventJobs = newEvents.map(event => ({ data: { event: serializeEvent(event) } }));
+    await eventQueue.addBulk(eventJobs);
+    return {
+      newEvents: newEvents.length,
+      lastSeenTxid,
+    };
+  });
 
-void balancesQueue.process(1, async () => {
-  const { stx, btc, xbtc } = await getBalances();
-  const message = `Balances: ${stx.stx} STX; ${btc.btc} BTC; ${xbtc.xbtc} xBTC`;
-  logger.info(
-    {
-      topic: 'balances',
+  void finalizeOutboundQueue.process(1, async () => {
+    return processPendingOutbounds(client);
+  });
+
+  void balancesQueue.process(1, async () => {
+    const { stx, btc, xbtc } = await getBalances();
+    const message = `Balances: ${stx.stx} STX; ${btc.btc} BTC; ${xbtc.xbtc} xBTC`;
+    logger.info(
+      {
+        topic: 'balances',
+        stx,
+        btc,
+        bridge: xbtc.xbtc,
+      },
+      message
+    );
+    return {
+      message,
       stx,
       btc,
       bridge: xbtc.xbtc,
-    },
-    message
-  );
-  return {
-    message,
-    stx,
-    btc,
-    bridge: xbtc.xbtc,
-  };
-});
+    };
+  });
 
-void finalizeOutboundQueue.add({}, { repeat: { every: 120_000 } });
-void eventCronQueue.add({}, { repeat: { every: 120_000 } });
-void balancesQueue.add({}, { repeat: { every: 60_000 } });
+  void finalizeOutboundQueue.add({}, { repeat: { every: 120_000 } });
+  void eventCronQueue.add({}, { repeat: { every: 120_000 } });
+  void balancesQueue.add({}, { repeat: { every: 60_000 } });
+}
